b983f6eabf2275fedd284d0f837e883a3c24c022
Administrator <672399171@qq.com>
2016-10-24 09:36:09 +0800

    c

--- demo1.go
+++ demo1.go
@@ -5,14 +5,27 @@ import (
 	"time"
 )
 
+var c, python, java bool
+
 func main() {
-	fmt.Printf("hello world %d", 5+1)
-	a, b := fun(2, 3)
+	// fmt.Printf("hello world %d\n", 5 + 1)
+	// a, b := fun(2, 3)
 
-	fmt.Print(a)
-	fmt.Print(b)
+	// fmt.Println(a, b)
 
-	fmt.Print(time.Now())
+	fmt.Println(time.Now())
+
+	var i int
+	fmt.Println(i, c, python, java)
+
+	sum := 1
+	/*for i := 0; i < 10; i++ {
+		sum += i
+	}*/
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
 }
 
 func fun(x, y int) (int, int) {
